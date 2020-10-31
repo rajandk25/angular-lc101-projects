dbf2b002c319a72dca66aa8aa1f1035bcd0ac3e9
rajandk25 <rajandk25@users.noreply.github.com>
2020-10-31 17:25:24 -0500

    finished exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Checkout these pictures';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = 'https://www.vippng.com/png/detail/394-3947569_random-image-from-user-small-t-rex-pixel.png';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQ2Uw3WAdvw989s07hf5tFl02LbYH-MrBFrw&usqp=CAU';
   image3 = 'https://404store.com/2018/09/01/random-small-images43.md.png';
+  image4 = 'https://media4.giphy.com/media/LmNwrBhejkK9EFP504/200.gif'
 
   constructor() { }
 
